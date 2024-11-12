f9f6bfaf7c299e4e481c89e24e271e97f96f80db
workspace014-admin <oscar.osorio1884@gmail.com>
2024-11-12 18:42:19 -0500

    Config static files

--- musicController/settings.py
+++ musicController/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "frontend" / "static",
+]
+
+
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
